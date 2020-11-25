e5d44d2bbf0e5bbf118d0eee315406e21c68d6b1
Drew Newman <drew.newman@integritystl.com>
2020-11-25 11:06:18 -0600

    S&F output and styling

--- wp-content/themes/wrcgroup-secure/page-news-template.php
+++ wp-content/themes/wrcgroup-secure/page-news-template.php
@@ -28,7 +28,83 @@ get_header();
             <?php echo do_shortcode('[searchandfilter slug="wrc-news"]'); ?>
           </div>
         </div>
-        <div class="news-filter-container">
+        <div class="news-results-container">
+          <?php
+
+          $args['search_filter_slug'] = "wrc-news";
+          $query = new WP_Query($args);
+          $total = $query->found_posts;
+          $totalPages = $query->max_num_pages;
+          
+          ?>
+
+          <div class="results-found" page_count="<?php echo $totalPages;?>"><span><?php echo $total; ?></span> Results Found</div>
+
+          <?php
+          if ( $query->have_posts() ) { ?>
+
+            <?php
+                echo '<ul class="news-list" id="results-list">';
+                while ( $query->have_posts() ) {
+                  $query->the_post();
+
+                  $title = get_the_title();
+                  $post_id = get_the_ID();
+                  $params = apply_filters( 'wp_terms_checklist_args', $args, $post_id );
+                  $current_site = get_field('site');
+                  $category_object = get_the_category($post_id);
+                  $category_name = $category_object[0]->name;
+                  $link = get_permalink();
+                  $imgObject = wp_get_attachment_image( get_post_thumbnail_id(), 'full' );
+                  $date = get_the_date('F j, Y');
+                  $summary = excerpt(60);
+                  ?>
+                  <li>
+                      <div class="featured-image-container">
+                        <?php echo '<a href="' . get_permalink() . '?current_site=' . $current_site . '">' . $imgObject . '</a>' ?>
+                        <h2 class="post_item_title"><?php echo '<a href="' . get_permalink() . '?current_site=' . $current_site . '">' . $title . '</a>' ?></h2>
+                      </div>
+                      <div class="post-meta">
+                        <span><?php echo $date ?></span>
+                        <span class="categories"><a class="archive-listing-category" href="#" data-category="<?php echo $category_object ?>"><?php echo $category_name ?></a></span>
+                        <?php 
+                          $tag_entries = get_the_tags();
+                          if(!empty($tag_entries)) {
+                            echo '<span class="tags">Tags:';
+
+                            $tagsNames = array();
+                            foreach($tag_entries as $tag_entry){
+                              $tagsNames[] = '<a class="archive-listing-tag" href="#" data-tag="' . $tag_entry->term_id . '">' . $tag_entry->name . '</a>';
+                            }
+                            $allTags = implode(',',$tagsNames );
+                            echo ' '. $allTags . '</span>';
+                          }
+                      ?>
+                      </div>
+                      <p>
+                        <?php echo $summary ?>
+                        <span class="read-more"><?php echo '<a href="' . get_permalink() . '?current_site=' . $current_site . '">Read More</a>' ?></span>
+                      </p>
+                  </li>
+                <?php }
+                echo '</ul>'; ?>
+
+                <div class="loading-screen">
+                  <div class="spinner"></div>
+                </div>
+                <div class="load-more-container hidden">
+                  <a href="#" id="load-more-link">Load More</a>
+                </div>
+                
+            <?php } else { ?>
+              <div class="no-results">
+                <h3>No Results Found</h3>
+              </div>
+            <?php }
+            /* Restore original Post Data */
+            wp_reset_postdata(); ?>
+        </div>
+        <!-- <div class="news-filter-container">
           <div class="tag-filter__container">
             <h4>Tags</h4>
             <i class="fa fa-caret-down" aria-hidden="true"></i>
@@ -57,7 +133,7 @@ get_header();
           <div class="load-more-container hidden">
             <a href="#" id="load-more-link">Load More</a>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
